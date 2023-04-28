c51627685c04c70582114ec7c80e53339a23944f
Denis <103854079+tkachukds@users.noreply.github.com>
2023-04-28 11:44:55 +0300

    full standart file

--- popup.js
+++ popup.js
@@ -1,6 +1,22 @@
-// получаем доступ к кнопке
+// получаем кнопку
 let pars = document.getElementById("btnparse");
 snow.addEventListener("click", async () => {
+onExtensionClick();
+alert('hi');
+});
 
+
+function convert(n){
+	var parts = n.split("/");
+	var gramm =  ( ( (parts.length == 1) ? parseInt(parts[0]) : parseInt(parts[0]) / parseInt(parts[1]) ) * 28.3495231 ).toFixed(2);
+	return  isNaN(gramm) ? "--" : gramm;
+}
+
+document.addEventListener('DOMContentLoaded', function() {	
+	chrome.tabs.executeScript( null, {"code": "window.getSelection().toString()"}, function(selection) {
+		  var selectedText = selection[0];
+		  var result = "";
+		  document.getElementById("output").innerHTML = selectedText;
+		});
 });
 
