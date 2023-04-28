7d1472c28dfdc3fa183ac9aca67657d29b23c41b
Denis <103854079+tkachukds@users.noreply.github.com>
2023-04-28 08:48:33 +0300

    added js

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
-alert('hi')
+chrome.runtime.sendMessage('start');
+btn.addEventListener("click", btnClick);
+let classes = "b-task-title-wrapper";
+let word = document.getElementsByClassName(classes).innerText;
+function btnClick() {
+     alert(word);
+}
